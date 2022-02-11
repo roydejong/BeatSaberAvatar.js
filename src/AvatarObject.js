6e22d92084fdfd1addf4c52ce0a3795d92c9da34
Roy de Jong <roy@softwarepunt.nl>
2022-02-11 22:40:22 +0100

    wip: avatar hands

--- src/AvatarObject.js
+++ src/AvatarObject.js
@@ -6,20 +6,31 @@ import AvatarEyes from "./AvatarEyes";
 export default class AvatarObject {
     constructor() {
         this.sceneGroup = new THREE.Group();
+
+        this.bodyGroup = new THREE.Group();
+        this.sceneGroup.add(this.bodyGroup);
+
+        this.handsGroup = new THREE.Group();
+        this.sceneGroup.add(this.handsGroup);
+
         this.loadedObjects = { };
 
         this.headMesh = new AvatarPartMesh("head", "AvatarHead.obj");
-        this.headTopMesh = AvatarPartsModel.headTops["BedHead"];
+        this.headTopMesh = AvatarPartsModel.headTops["PoloCap"];
         this.eyes = new AvatarEyes("test");
+        this.handsMesh = AvatarPartsModel.hands["Hand"];
     }
 
     load(loadingManager, assetsBaseDir) {
-        this.handlePartLoaded("headMesh", null);
-        this.handlePartLoaded("headTopMesh", null);
+        this.handlePartLoaded("headMesh", null, this.sceneGroup);
+        this.handlePartLoaded("headTopMesh", null, this.sceneGroup);
+        this.handlePartLoaded("eyes", null, this.sceneGroup);
+        this.handlePartLoaded("handLeft", null, this.handsGroup);
+        this.handlePartLoaded("handRight", null, this.handsGroup);
 
         if (this.headMesh) {
             this.headMesh.load(loadingManager, assetsBaseDir, (meshObj) => {
-                this.handlePartLoaded("headMesh", meshObj);
+                this.handlePartLoaded("headMesh", meshObj, this.sceneGroup);
                 this.setPartColors("headMesh", "#ff0000");
                 meshObj.position.z = .125;
             });
@@ -28,26 +39,44 @@ export default class AvatarObject {
         if (this.headTopMesh) {
             this.headTopMesh.load(loadingManager, assetsBaseDir, (meshObj) => {
                 // TODO Position / rotation
-                this.handlePartLoaded("headTopMesh", meshObj);
+                this.handlePartLoaded("headTopMesh", meshObj, this.sceneGroup);
                 this.setPartColors("headTopMesh", "#0000ff");
             });
         }
 
         if (this.eyes) {
             this.eyes.load(loadingManager, assetsBaseDir, (planeObj) => {
-                this.handlePartLoaded("eyes", planeObj);
+                this.handlePartLoaded("eyes", planeObj, this.sceneGroup);
                 planeObj.position.z = .153;
                 planeObj.position.y = .025;
             });
         }
+
+        if (this.handsMesh) {
+            this.handsMesh.load(loadingManager, assetsBaseDir, (meshObj) => {
+
+                let handLeft = meshObj.clone();
+                handLeft.scale.set(1, 1, 1);
+                handLeft.position.set(-0.333, -.333, .1);
+
+                let handRight = meshObj.clone();
+                handRight.scale.set(-1, 1, 1);
+                handRight.position.set(.333, -.333, .1);
+
+                this.handlePartLoaded("handLeft", handLeft, this.handsGroup);
+                this.handlePartLoaded("handRight", handRight, this.handsGroup);
+                this.setPartColors("handLeft", "#ff0000");
+                this.setPartColors("handRight", "#ff0000");
+            });
+        }
     }
 
-    handlePartLoaded(key, gameObject) {
+    handlePartLoaded(key, gameObject, group) {
         if (this.loadedObjects[key]) {
-            this.sceneGroup.remove(this.loadedObjects[key]);
+            group.remove(this.loadedObjects[key]);
         }
         if (gameObject) {
-            this.sceneGroup.add(gameObject);
+            group.add(gameObject);
             this.loadedObjects[key] = gameObject;
         }
     }
--- src/AvatarPartsModel.js
+++ src/AvatarPartsModel.js
@@ -4,6 +4,8 @@ export default class AvatarPartsModel {
     static init() {
         this.headTops = { };
         this.registerHeadTops();
+        this.hands = { };
+        this.registerHands();
     }
 
     static registerHeadTops() {
@@ -12,9 +14,12 @@ export default class AvatarPartsModel {
             "SweatBand", "Untidy", "WetHair", "WindSwept", "WinterHat", "Wizard"];
 
         headTopIds.forEach(headTopId => {
-            let headTopMesh = new AvatarPartMesh(headTopId, `HeadTop/${headTopId}_01.obj`);
-            this.headTops[headTopId] = headTopMesh;
-            console.log('[registerHeadTops]', headTopMesh.id, '->', headTopMesh.meshPath)
+            this.headTops[headTopId] = new AvatarPartMesh(headTopId, `HeadTop/${headTopId}_01.obj`);
         });
+    }
+
+    static registerHands() {
+        this.hands["Hand"] = new AvatarPartMesh("Hand", "Hands/MeshHand01.obj");
+        this.hands["HandGloves"] = new AvatarPartMesh("HandGloves", "Hands/MeshHandGloves01_v02.obj");
     }
 }
