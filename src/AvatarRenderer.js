c4a50639bf4f866cf4d6bf9bff17e364e163c71e
Roy de Jong <roy@softwarepunt.nl>
2022-02-12 02:18:57 +0100

    wip: responsive aspect ratio / resize handling

--- src/AvatarRenderer.js
+++ src/AvatarRenderer.js
@@ -5,33 +5,41 @@ import {TrackballControls} from 'three/examples/jsm/controls/TrackballControls.j
 const lightDebugOn = false;
 
 export default class AvatarRenderer {
+    // -----------------------------------------------------------------------------------------------------------------
+    // Setup
+
     init(domElement, assetsBaseDir = "/assets/") {
         if (this.didInit) {
             throw new Error("Already initialized!");
         }
 
+        this.domElement = domElement;
         this.assetsBaseDir = assetsBaseDir;
 
+        // Create scene
         this.scene = new THREE.Scene();
 
-        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 10);
-        this.camera.position.set(0, 0, 1.5);
-        this.camera.rotation.set(0, 0, 0);
-
+        // Create renderer
         this.renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.setClearColor(0x000000, 0);
         this.renderer.shadowMap.enabled = true;
         this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
+        // Create camera
+        window.addEventListener( 'resize', this._updateCamera.bind(this), false);
+        this._updateCamera();
+
+        // Add renderer to DOM, replacing its contents
         domElement.innerHTML = "";
         domElement.appendChild(this.renderer.domElement);
 
+        // Set up mouse/keyboard controls
         this.controls = new TrackballControls(this.camera, this.renderer.domElement);
         this.controls.rotateSpeed = 10;
         this.controls.noPan = true;
         this.controls.target.set(0, 0, 0);
 
+        // Start render loop
         this._spawnDirectionalLight();
 
         console.log('[BeatSaberAvatar.js]', 'Created avatar renderer', this.renderer);
@@ -40,21 +48,43 @@ export default class AvatarRenderer {
         this._renderLoop();
     }
 
-    setRandomAvatar() {
-        this.setAvatarData(null);
-    }
+    _updateCamera() {
+        const aspectRatio = this.domElement.clientWidth / this.domElement.clientHeight;
 
-    setAvatarData(avatarData) {
-        console.log('[BeatSaberAvatar.js]', 'Set avatar data:', avatarData || "Random!");
-
-        if (this.avatarObject == null) {
-            this.avatarObject = new AvatarObject();
-            this.scene.add(this.avatarObject.sceneGroup);
+        if (!this.camera) {
+            this.camera = new THREE.PerspectiveCamera(60, aspectRatio, 0.1, 10);
+        } else {
+            this.camera.aspect = aspectRatio;
         }
 
-        this.avatarObject.setAvatarData(avatarData);
-        this.avatarObject.load(THREE.DefaultLoadingManager, this.assetsBaseDir);
+        this.camera.position.set(0, 0, 1.5);
+        this.camera.rotation.set(0, 0, 0);
+
+        if (this.renderer) {
+            this.renderer.setSize(this.domElement.clientWidth, this.domElement.clientHeight);
+        }
+
+        this.camera.updateProjectionMatrix();
     }
+
+    _spawnDirectionalLight() {
+        this.light = new THREE.DirectionalLight(0xffffff, 1);
+        this.light.castShadow = true;
+        this.scene.add(this.light);
+
+        this.light.shadow.mapSize.width = 512;
+        this.light.shadow.mapSize.height = 512;
+        this.light.shadow.camera.near = 0.1;
+        this.light.shadow.camera.far = 10;
+
+        if (lightDebugOn) {
+            const helper = new THREE.CameraHelper(this.light.shadow.camera);
+            this.scene.add(helper);
+        }
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Render loop
 
     _renderLoop() {
         requestAnimationFrame(this._renderLoop.bind(this));
@@ -72,19 +102,22 @@ export default class AvatarRenderer {
         this.renderer.render(this.scene, this.camera);
     }
 
-    _spawnDirectionalLight() {
-        this.light = new THREE.DirectionalLight(0xffffff, 1);
-        this.light.castShadow = true;
-        this.scene.add(this.light);
+    // -----------------------------------------------------------------------------------------------------------------
+    // Public API
 
-        this.light.shadow.mapSize.width = 512;
-        this.light.shadow.mapSize.height = 512;
-        this.light.shadow.camera.near = 0.1;
-        this.light.shadow.camera.far = 10;
+    setRandomAvatar() {
+        this.setAvatarData(null);
+    }
 
-        if (lightDebugOn) {
-            const helper = new THREE.CameraHelper(this.light.shadow.camera);
-            this.scene.add(helper);
+    setAvatarData(avatarData) {
+        console.log('[BeatSaberAvatar.js]', 'Set avatar data:', avatarData || "Random!");
+
+        if (this.avatarObject == null) {
+            this.avatarObject = new AvatarObject();
+            this.scene.add(this.avatarObject.sceneGroup);
         }
+
+        this.avatarObject.setAvatarData(avatarData);
+        this.avatarObject.load(THREE.DefaultLoadingManager, this.assetsBaseDir);
     }
 }
