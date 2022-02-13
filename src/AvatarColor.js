39d632a313759023b0d2dafcabc4313bb6f0e1b9
Roy de Jong <roy@softwarepunt.nl>
2022-02-13 16:39:35 +0100

    wip: fix random color generation

--- src/AvatarColor.js
+++ src/AvatarColor.js
@@ -5,6 +5,6 @@ export default class AvatarColor {
     }
 
     static getRandomColorValue() {
-        return '#' + (Math.floor(Math.random()*16777215).toString(16));
+        return '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
     }
 }
