16a0cf5dac4ec18d38427b9c308dd1867eec9703
Roy de Jong <roy@softwarepunt.nl>
2022-02-14 04:37:26 +0100

    fix(AvatarColor): gracefully handle hex strings with alpha

--- src/AvatarColor.js
+++ src/AvatarColor.js
@@ -3,6 +3,12 @@ import * as THREE from 'three';
 export default class AvatarColor {
     constructor(id, color) {
         this.id = id;
+
+        if (typeof color === 'string' && color.startsWith('#')) {
+            // Cut off alpha component for colors, Three.js doesn't support or understand it
+            color = color.substring(0, 7);
+        }
+
         this.color = new THREE.Color(color);
     }
 
